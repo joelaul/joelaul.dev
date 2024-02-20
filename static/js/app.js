5926a4b49c61f2037cb928737c7a8b410f0215eb
Joe <laulettaj@gmail.com>
2024-02-20 16:35:39 -0500

    add social icon hover game

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,3 @@
-// IMPORT
-
-// import { typewriter } from './typewriter';
-
 // DOM
 
 const body = document.querySelector('body');
@@ -10,17 +6,18 @@ const h1 = document.querySelector('.h1');
 const socialEls = document.querySelectorAll('.user-social > a');
 const socialRow = document.querySelector('.row.social');
 const desc = document.querySelector('.user-description');
+let cta;
 
 // STATE
 
 const state = {
-    descText: `🎸👨‍💻 I'm a guitarist and recording artist turned fullstack developer. I like building things that awaken people's curiosity.`
+    descText: `🎸👨‍💻 I'm a guitarist and recording artist turned fullstack developer. I like building things that awaken people's curiosity.`,
+    hoveredSocialEls: []
 }
 
-// FUNCTIONS
+// HELPERS
 
 const typewriter = (element, text, ms) => {
-    // if (!localStorage['typewriterDone']) {
         desc.textContent = '';
         let index = 0;
 
@@ -32,48 +29,81 @@ const typewriter = (element, text, ms) => {
                 clearInterval(interval);
             }
         }, ms)
-        // localStorage.setItem('typewriterDone', 'true');
-   /*  } else {
-        desc.textContent = state.descText;
-    } */
+}
+
+const isUniqueSocialEl = (el) => {
+    return state.hoveredSocialEls.indexOf(el) == -1;
+}
+
+const renderCallToAction = (ms) => {
+    setTimeout(() => {
+        desc.innerHTML += `
+        <div>
+            <a class="cta" href="/portfolio" target="_blank">
+                Check out some of my work!
+            </a>
+        </div>
+        `
+    }, ms);
+
+    cta = document.querySelector('.cta');
 }
 
 // HANDLERS
+
+const handleSocialElMouseover = (el) => {
+    let audio = new Audio(`audio/${el.title.toLowerCase()}.wav`);
+    audio.setAttribute('aria-muted', 'false');
+    audio.play();
+
+    h1.textContent = el.title;
+    el.firstChild.style.color = el.classList[2];
+    h1.style.color = el.classList[2];
+
+    if (el.classList[1] == 'resume') {
+        el.firstChild.src = "../img/resume-hover.png"
+    }
+
+    if (state.hoveredSocialEls.length < 5 && isUniqueSocialEl(el)) {
+        state.hoveredSocialEls.push(el);
+        if (state.hoveredSocialEls.length == 5) {
+            renderCallToAction(0);
+        }
+    } else {}
+}
+
+const handleMeClick = () => {
+    me.classList.toggle('spin');
+    for (let el of socialEls) {
+        el.firstChild.style.color = '#333333';
+        if (el.classList[1] == 'resume') {
+            el.firstChild.src = "../img/resume.png"
+        }
+        el.classList.toggle('spin');
+    }
+}
 
 // INIT
 
 const init = () => {
     window.addEventListener('load', () => {
-        typewriter(desc, state.descText, 30)
+        typewriter(desc, state.descText, 25)
     });
+
     for (let el of socialEls) {
         el.firstChild.addEventListener('mouseover', () => {
-            h1.textContent = el.title;
-            let audio = new Audio(`audio/${el.title.toLowerCase()}.wav`);
-            audio.setAttribute('aria-muted', 'false');
-            audio.play();
-            el.firstChild.style.color = el.classList[2];
-            h1.style.color = el.classList[2];
-            if (el.classList[1] == 'resume') {
-                el.firstChild.src = "resume-hover.png"
-            }
+            handleSocialElMouseover(el);
         });
     };
+
     socialRow.addEventListener('mouseout', () => {
         h1.textContent = 'Joe Lauletta';
         h1.style.color = 'black';
     });
-    me.addEventListener('click', () => {
-        me.classList.toggle('spin');
-        for (let el of socialEls) {
-            el.firstChild.style.color = '#333333';
-            if (el.classList[1] == 'resume') {
-                el.firstChild.src = "resume.png"
-            }
-            el.classList.toggle('spin');
-        }
-    });
+
+    me.addEventListener('click', handleMeClick);
 }
+
 
 init();
 
